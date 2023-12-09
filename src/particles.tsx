43eb158d0f68ce1872d24c4e6c687b5bf303ae3a
Rafael <rgarrido846@gmail.com>
2023-12-09 12:37:53 -0300

    feat: create particles effect and style

---
+++ src/particles.tsx
@@ -0,0 +1,73 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+const Particle = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+
+    await loadSlim(engine);
+}, [])
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      console.error(container);
+    },
+    []
+  );
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+
+        fpsLimit: 120,
+
+        particles: {
+          number: {
+            value: 160,
+            density: {
+              enable: true,
+              value_area: 1500,
+            },
+          },
+          line_linked: {
+            enable: false,
+            opacity: 0.03,
+          },
+          move: {
+            direction: "right",
+            speed: 0.05,
+          },
+          size: {
+            value: 1,
+          },
+          opacity: {
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.05,
+            },
+          },
+        },
+        interactivity: {
+          events: {
+            onclick: {
+              enable: true,
+              mode: "push",
+            },
+          },
+          modes: {
+            push: {
+              particles_nb: 1,
+            },
+          },
+        },
+        retina_detect: true,
+      }}
+    />
+  );
+};
+
+export default Particle;
